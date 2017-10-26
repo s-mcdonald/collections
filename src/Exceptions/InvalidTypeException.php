76bfefc01b2aa4f828a17b79819a62a4b033e76e
Sam McDonald <s.mcdonald@outlook.com.au>
2017-10-26 12:58:00 +1000

    fix namespace casing

--- src/Exceptions/InvalidTypeException.php
+++ src/Exceptions/InvalidTypeException.php
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-namespace SamMcdonald\Collections\Exceptions;
+namespace SamMcDonald\Collections\Exceptions;
 
 class InvalidTypeException extends \Exception 
 {
